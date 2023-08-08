0cc4ca9c4a26798fad81beda166a765bd93a74f0
Trifonov Denys <trifonowden123@gmail.com>
2023-08-08 21:05:40 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,20 +1,49 @@
 import css from './ContactForm.module.css'
 import { useDispatch } from "react-redux";
-// import { addContact } from 'redux/contactsSlice';
 import { addContact } from 'redux/operations';
+import { useSelector } from "react-redux";
+import { getContacts } from "redux/selectors";
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const ContactForm = () => {
   const dispatch = useDispatch()
+  const contacts = useSelector(getContacts);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const form = e.target
+    if (contacts.some(({ name }) => name === form.elements.name.value)) {
+      form.reset()
+      return  toast('contact alredy added', {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        });
+    }
     dispatch(addContact({name:form.elements.name.value, phone:form.elements.number.value}))
     form.reset()
+      toast('contact added', {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
   };
 
   return (
-    <form className={css.form} onSubmit={handleSubmit}>
+    <>
+      <ToastContainer style={{fontSize: 20}}/>
+      <form className={css.form} onSubmit={handleSubmit}>
       <label className={css.lable} htmlFor="name">Name</label>
       <input 
         className={css.input}
@@ -32,6 +61,8 @@ const ContactForm = () => {
       />
       <button type="submit">Add contact</button>
     </form>
+    </>
+    
   );
 }
 
